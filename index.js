7aa9d9be9c4874feac124a23c18b41813843b571
Robert Nagy <ronag89@gmail.com>
2017-02-19 11:55:09 +0100

    Prefer Object.create(null) over '$' prefixing.

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ function mixin(obj) {
 
 Emitter.prototype.on =
 Emitter.prototype.addEventListener = function(event, fn){
-  this._callbacks = this._callbacks || {};
-  (this._callbacks['$' + event] = this._callbacks['$' + event] || [])
+  this._callbacks = this._callbacks || Object.create(null);
+  (this._callbacks[event] = this._callbacks[event] || [])
     .push(fn);
   return this;
 };
@@ -84,21 +84,21 @@ Emitter.prototype.off =
 Emitter.prototype.removeListener =
 Emitter.prototype.removeAllListeners =
 Emitter.prototype.removeEventListener = function(event, fn){
-  this._callbacks = this._callbacks || {};
+  this._callbacks = this._callbacks || Object.create(null);
 
   // all
   if (0 == arguments.length) {
-    this._callbacks = {};
+    this._callbacks = Object.create(null);
     return this;
   }
 
   // specific event
-  var callbacks = this._callbacks['$' + event];
+  var callbacks = this._callbacks[event];
   if (!callbacks) return this;
 
   // remove all handlers
   if (1 == arguments.length) {
-    delete this._callbacks['$' + event];
+    delete this._callbacks[event];
     return this;
   }
 
@@ -123,9 +123,9 @@ Emitter.prototype.removeEventListener = function(event, fn){
  */
 
 Emitter.prototype.emit = function(event){
-  this._callbacks = this._callbacks || {};
+  this._callbacks = this._callbacks || Object.create(null);
   var args = [].slice.call(arguments, 1)
-    , callbacks = this._callbacks['$' + event];
+    , callbacks = this._callbacks[event];
 
   if (callbacks) {
     callbacks = callbacks.slice(0);
@@ -146,8 +146,8 @@ Emitter.prototype.emit = function(event){
  */
 
 Emitter.prototype.listeners = function(event){
-  this._callbacks = this._callbacks || {};
-  return this._callbacks['$' + event] || [];
+  this._callbacks = this._callbacks || Object.create(null);
+  return this._callbacks[event] || [];
 };
 
 /**
